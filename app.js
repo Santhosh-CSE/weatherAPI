a765eb8454311fa51d42eba422118d8eb6066922
Santhosh-CSE <190030905cse@gmail.com>
2022-09-05 10:59:08 +0530

    first commit

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ var request = require('request');
 let url = `https://api.openweathermap.org/data/2.5/weather?`
 +`q=${city}&appid=7a136a981d423dc95f750eda74e46281`
 
-request({ url: url, json: true }, function (error, response) {
-if (error)
+request({ url: url, json: true }, function (err, response) {
+if (err)
 {
-console.log('Unable to connect to Forecast API');
+console.log('Unable to Forecast the Weather Conditions');
 } else {
 console.log('It is currently '
 + response.body.main.temp
@@ -29,8 +29,8 @@ console.log('Humidity today is '
 );
 res.send(response.body);
 }
-})
-})
+});
+});
 app.listen(3000, function() {
 console.log('Currently running on port 3000');
 });
