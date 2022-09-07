d8e22beeb165b71da5f9934747c275729d9067f4
Santhosh-CSE <190030905cse@gmail.com>
2022-09-07 08:51:24 +0530

    first commit

--- app.js
+++ app.js
@@ -1,35 +1,65 @@
-const request = require('request');
+const redis = require('redis');
+const request = require('request'); 
 const express = require('express');
 const app = express();
-
-app.get('/', function(req, res) {
-// GET Method w.r.t City
-var city = req.query.city;
-var request = require('request');
-
-let url = `https://api.openweathermap.org/data/2.5/weather?`+`q=${city}&appid=7a136a981d423dc95f750eda74e46281`
-
-request({ url: url, json: true }, function (err, response) {
-if (err)
-{
-console.log('Unable to Forecast the Weather Conditions');
-} else {
-console.log('It is currently '
-+ response.body.main.temp
-+ ' degrees out.'
-);
-console.log('The high today in '+city+' is '
-+ response.body.main.temp_max
-+ ' with a low of '
-+ response.body.main.temp_min
-);
-console.log('Humidity today is '
-+ response.body.main.humidity
-);
-res.send({temp:response.body.main.temp});
-}
+const redisPort = 6379
+const client = redis.createClient();
+client.on("error", (err) => {
+    console.log(err);
 });
+let API_KEY = '7a136a981d423dc95f750eda74e46281'; 
+  let stringify;
+  app.get('/', async(req,res)=> {
+    if(!req.query.city){
+      const message = 'Invalid Request';
+        res.status(403).send(message);
+    }
+
+    var resp = await getdatafromRedis(req.query.city);
+    //console.log("value from redis", resp); process.exit(0);
+    if(resp != null){
+        res.status(200).send(resp)
+    }else{
+       getDatafromAPI(req.query.city, (err, resp) => {
+            if(err){
+                res.send(err);
+            }else{
+                res.status(200).send(resp);
+            }
+        });
+    }
+})
+  
+app.listen(3000,async ()=> {
+    await client.connect();
+    console.log('Weather app listening on port 3000!');
 });
-app.listen(3000, function() {
-console.log('Currently running on port 3000');
-});
+
+async function getdatafromRedis(city){
+    console.log("I'm in redis function");
+    const result = await client.get(city);
+    if(result !=null){
+        return JSON.parse(result);
+    } else {
+        return null;
+    }
+    }
+
+ function getDatafromAPI(city,callback){
+    console.log("I'm in api function");
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" +city+ "&appid=" + API_KEY;
+    request({ url:url, json: true },(error, response)=> { 
+        if (error) 
+        {  
+            callback({"msg":"error"},null); 
+        } 
+          else { 
+            client.set(city,JSON.stringify({"temp":response.body.main.temp}));
+     //console.log('It is currently ' + response.body.main.temp+ ' degrees out.'  ); 
+      //  console.log('The high today in '+city+' is ' + response.body.main.temp_max + ' with a low of '+ response.body.main.temp_min ); 
+           // console.log('Humidity today is ' + response.body.main.humidity  ); 
+            callback (null, {"temp":response.body.main.temp});
+            
+        } 
+    });
+ }
